b19da42c46edc6f3d368131abf93e1fb4ecae900
Artem Belkov <bestk1ngarthur@aol.com>
2021-03-05 19:22:20 +0300

    Initial Commit

---
+++ Layout/Layout.h
@@ -0,0 +1,18 @@
+//
+//  Layout.h
+//  Layout
+//
+//  Created by Artem Belkov on 05.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Layout.
+FOUNDATION_EXPORT double LayoutVersionNumber;
+
+//! Project version string for Layout.
+FOUNDATION_EXPORT const unsigned char LayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layout/PublicHeader.h>
+
+
